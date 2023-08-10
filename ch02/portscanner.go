df4e0a8720489a1117854c77637f5c4391716b79
ms419 <d3vcare@gmail.com>
2023-08-10 12:39:29 +0330

    ...

--- ch02/portscanner.go
+++ ch02/portscanner.go
@@ -20,17 +20,21 @@ func NonConcurrentScan() {
 
 
 func main() {
-	// NonConcurrentScan()
+	//NonConcurrentScan()
 
 	// Concurrent scan, naive way
 	var waiter sync.WaitGroup
+	var address string
+	fmt.Print("Target Address: ")
+	fmt.Scanln(&address)
+	openPorts := make([]int16, 0, 0)
 	for i := int16(1); i < 1024; i++ {
 		waiter.Add(1) // increment the counter of wait group to inform that there is new routine to wait for
 		go func(port int16) {
 			defer waiter.Done() // decrease the wait counter after the routine is finished
-			const address string = "127.0.0.1"
 			if connection, err := net.Dial("tcp", fmt.Sprintf("%s:%d", address, port)); err == nil {
 				fmt.Printf("Port #%d is open\n", port)
+				openPorts = append(openPorts, port)
 				connection.Close()
 			} else {
 				fmt.Printf("Port #%d is closed or filtered: %s\n", port, err)
@@ -38,5 +42,5 @@ func main() {
 		}(i)
 	}
 	waiter.Wait() // wait until the wait griup counter becomes zero
-
+	fmt.Println("---------------------------------------------------\nOpen Ports are:\t", openPorts)
 }
