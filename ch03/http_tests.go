198c9125ea04be8e12e39d7a9d036b3b2d908016
pya-h <p4ya.py@gmail.com>
2023-08-23 18:04:20 +0330

    ...

--- ch03/http_tests.go
+++ ch03/http_tests.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
+func ExtractResponse(res *http.Response) {
+	// response Body is of type ReadCloser
+	// u need to close it after using
+	defer res.Body.Close()
+	log.Println(res.Status)
+	log.Println(res.StatusCode)
+	if body, err := ioutil.ReadAll(res.Body); err == nil {
+		log.Println(string(body))
+	} else {
+		log.Panicln(err)
+	}
+	fmt.Println("-----------------------------------------------------------------------------------------------------")
+}
+
 func main() {
 	if res, err := http.Get("http://google.com/robots.txt"); err == nil {
 		defer res.Body.Close()
-		log.Println(res)
+		ExtractResponse(res)
 	} else {
-		log.Println(err)
+		log.Println("GET:", err)
 	}
 
-	if res, err  := http.Head("http://google.com/robots.txt"); err == nil {
+	if res, err := http.Head("http://google.com/robots.txt"); err == nil {
+		// response Body is of type ReadCloser
+		// u need to close it after using
 		defer res.Body.Close()
-		log.Println(res)
+		ExtractResponse(res)
 	} else {
-		log.Println(err)
+		log.Println("HEAD:", err)
 	}
 
 	form := url.Values{}
@@ -28,17 +46,44 @@ func main() {
 
 	if res, err := http.Post("http://google.com/robots.txt",
 		"application/x-www-form-urlencoded", strings.NewReader(form.Encode())); err == nil {
-			defer res.Body.Close()
-			log.Println(res)
+		// response Body is of type ReadCloser
+		// u need to close it after using
+		defer res.Body.Close()
+		ExtractResponse(res)
 	} else {
-		log.Println(err)
+		log.Println("POST:", err)
 	}
 
 	// or use FormPost
 	if res, err := http.PostForm("http://google.com/robots.txt", form); err == nil {
+		// response Body is of type ReadCloser
+		// u need to close it after using
 		defer res.Body.Close()
-		log.Println(res)
+		ExtractResponse(res)
 	} else {
-		log.Println(err)
+		log.Println("PostForm: ", err)
+	}
+
+	var client http.Client
+
+	if req, err := http.NewRequest("DELETE", "http://google.com/robots.txt", nil); err == nil {
+		if res, err := client.Do(req); err == nil {
+			ExtractResponse(res)
+		} else {
+			log.Println("DELETE: ", err)
+
+		}
+	}
+
+	form.Add("Another", "Foo")
+
+	if req, err := http.NewRequest("PUT", "http://google.com/robots.txt", strings.NewReader(form.Encode())); err == nil {
+		if res, err := client.Do(req); err == nil {
+
+			ExtractResponse(res)
+		} else {
+			log.Println("PUT: ", err)
+
+		}
 	}
 }
